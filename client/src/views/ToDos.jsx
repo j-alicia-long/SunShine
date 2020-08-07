8b4764337f331badc58b999b30cdad57ae6681c1
Jason Au <jau8@gatech.edu>
2020-08-07 01:32:03 -0400

    added polls

--- client/src/views/ToDos.jsx
+++ client/src/views/ToDos.jsx
@@ -20,7 +20,56 @@ import { Grid, Row, Col, Alert } from "react-bootstrap";
 import CustomCheckbox from "components/CustomCheckbox/CustomCheckbox";
 import * as typeformEmbed from '@typeform/embed';
 
+import Poll from 'react-polls';
+ 
+// Declaring poll question and answers
+// STYLING: https://github.com/viniciusmeneses/react-polls/blob/master/src/index.js
+const pollQuestion1 = 'Are you a member of one or more PODs?'
+const pollAnswers1 = [
+  { option: 'Recruitment', votes: 5 },
+  { option: 'Representation', votes: 2 },
+  { option: 'Retention', votes: 3 }
+]
+
+const pollQuestion2 = 'Which of the following three pillars for D&I needs the most improvement at VMware?'
+const pollAnswers2 = [
+  { option: 'Yes', votes: 8 },
+  { option: 'No', votes: 2 }
+]
+
+const pollQuestion3 = 'On a daily basis, do you have lunch/social hangouts with people that are not in your ethnic background?'
+const pollAnswers3 = [
+  { option: 'Yes', votes: 21 },
+  { option: 'No', votes: 13 }
+]
+
+const pollQuestion4 = 'Features improvements for spaces on social.vmware.com'
+const pollAnswers4 = [
+  { option: 'Formal POD Onboarding', votes: 21 },
+  { option: 'Event promotion', votes: 13 },
+  { option: 'Event attendance tracking', votes: 13 },
+  { option: 'Region membership filters', votes: 13 },
+  { option: 'POD officers & points of contact page', votes: 13 }
+]
+
 class ToDos extends Component {
+  state = {
+    pollAnswers1: [...pollAnswers1]
+  }
+ 
+  // Handling user vote
+  // Increments the votes count of answer when the user votes
+  handleVote = voteAnswer => {
+    const { pollAnswers1 } = this.state
+    const newPollAnswers = pollAnswers1.map(answer => {
+      if (answer.option === voteAnswer) answer.votes++
+      return answer
+    })
+    this.setState({
+      pollAnswers: newPollAnswers
+    })
+  }
+
   constructor(props) {
     super(props);
     this.state = {
@@ -90,6 +139,20 @@ class ToDos extends Component {
                   })}
                 </Col>
               </Row>
+              <Row>
+                <Col lg={3}>
+                    <Poll question={pollQuestion1} answers={pollAnswers1} onVote={this.handleVote} />
+                </Col>
+                <Col lg={3}>
+                    <Poll question={pollQuestion2} answers={pollAnswers2} onVote={this.handleVote} />
+                </Col>
+                <Col lg={3}>
+                    <Poll question={pollQuestion3} answers={pollAnswers3} onVote={this.handleVote} />
+                </Col>
+                <Col lg={3}>
+                    <Poll question={pollQuestion4} answers={pollAnswers4} onVote={this.handleVote} />
+                </Col>
+            </Row>
             </div>
           </div>
         </Grid>
